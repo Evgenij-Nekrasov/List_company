b43491b0c329c83850064e0568c9392d3f17600a
Evgenij Nekrasov <nekrasovevgenij3@gmail.com>
2024-07-04 13:28:41 +0300

    The app was created using React, Redux/Toolkit, TS and Taiwind

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   images: {
+      remotePatterns: [
+         {
+            protocol: "https",
+            hostname: "res.cloudinary.com",
+            port: "",
+            pathname:
+               "/speedwares/image/upload/v1659284687/windframe-logo-main_daes7r.png",
+         },
+         {
+            protocol: "https",
+            hostname: "static01.nyt.com",
+            port: "",
+            pathname:
+               "/images/2019/11/08/world/08quebec/08quebec-superJumbo.jpg",
+         },
+      ],
+   },
+};
 
 export default nextConfig;
